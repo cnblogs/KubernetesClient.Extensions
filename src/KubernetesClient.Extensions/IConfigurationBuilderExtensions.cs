3e390138c731030cd4f11ff4f790d519be8ad0df
dudu <dudu@cnblogs.com>
2021-01-23 12:47:11 +0800

    Change namespace

--- src/KubernetesClient.Extensions/IConfigurationBuilderExtensions.cs
+++ src/KubernetesClient.Extensions/IConfigurationBuilderExtensions.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using k8s;
 using Microsoft.Extensions.Configuration;
 
-namespace k8s.Extensions
+namespace Microsoft.Extensions.Configuration
 {
     public static class ConfigMapExtensions
     {
